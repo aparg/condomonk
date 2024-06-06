238f8ff38c9aff2657d24e26857594bfa485284f
iam8uman <me8848you@gmail.com>
2024-06-06 15:59:01 +0545

    Dynamic card along with data

--- src/components/CardContent.js
+++ src/components/CardContent.js
@@ -1,28 +1,58 @@
-import Image from "next/image";
+// import Image from "next/image";
+// import React from "react";
+
+// const CardContent = () => {
+//   return (
+//     <div className="flex flex-col w-full flex-wrap gap-3">
+//       <div className="img">
+//         <img
+//           src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+//           alt="Laptop"
+//           className="h-[200px] w-full object-cover"
+//         />
+//       </div>
+//       <div className="mini-title text-sm font-light uppercase">
+//         Unique Living
+//       </div>
+//       <div className="title text-xl w-full playfair">
+//         How to Assign a Condo Unit FAST - Don't Walk Away Until You Watch This!
+//       </div>
+//       <div className="subtitle font-light text-sm text-gray-500">
+//         At the very beginning of 2023, I published this video in response to
+//         news headlines saying “Record Number of Condos to Flood Toronto...
+//       </div>
+//     </div>
+
+//   );
+// };
+
+// export default CardContent;
+
+
+
 import React from "react";
 
-const CardContent = () => {
+const CardContent = React.forwardRef(({ imgSrc, miniTitle, title, subtitle},ref) => {
   return (
-    <div className="flex flex-col w-full flex-wrap gap-3">
+    <div className="flex flex-col w-full flex-wrap gap-3" ref={ref}>
       <div className="img">
         <img
-          src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+          src={imgSrc}
           alt="Laptop"
           className="h-[200px] w-full object-cover"
         />
       </div>
       <div className="mini-title text-sm font-light uppercase">
-        Unique Living
+        {miniTitle}
       </div>
       <div className="title text-xl w-full playfair">
-        How to Assign a Condo Unit FAST - Don't Walk Away Until You Watch This!
+        {title}
       </div>
       <div className="subtitle font-light text-sm text-gray-500">
-        At the very beginning of 2023, I published this video in response to
-        news headlines saying “Record Number of Condos to Flood Toronto...
+        {subtitle}
       </div>
     </div>
   );
-};
+})
 
 export default CardContent;
